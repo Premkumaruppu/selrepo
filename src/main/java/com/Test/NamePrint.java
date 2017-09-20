b5b061234b3991205b10c5a60727a8d045dcd389
Premkumaruppu <premkumar_uppu@outlook.com>
2017-09-20 18:15:53 +0530

    "I am Going to Commit NamePrint"
    Change-Id: Ia736b9807779e5009a5cfe26674b9c2c04505316
    Signed-off-by: Premkumaruppu <premkumar_uppu@outlook.com>

---
+++ src/main/java/com/Test/NamePrint.java
@@ -0,0 +1,11 @@
+package com.Test;
+
+public class NamePrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Premkumar Uppu");
+		
+	}
+
+}
